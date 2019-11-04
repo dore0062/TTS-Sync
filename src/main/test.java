4e84cd6db96161d2ad92740e4207a6dba3a28f49
Skeeto <skeevyskeeve@gmail.com>
2019-11-04 03:10:33 -0500

    Deleted test files

--- src/main/test.java
+++
@@ -1,9 +0,0 @@
-import com.intellij.openapi.components.ServiceManager;
-import com.intellij.openapi.project.Project;
-import org.jetbrains.annotations.NotNull;
-
-public interface test {
-    static test getInstance(@NotNull Project project) {
-        return ServiceManager.getService(project, test.class);
-    }
-}
